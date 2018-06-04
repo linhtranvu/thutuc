15301adb276e0c03e5921f1194c8e3f9f8f24782
linhtranvu <linhtranvu@gmail.com>
2018-06-04 16:31:48 +0700

    fix ngay 04.6

--- thutuc.js
+++ thutuc.js
@@ -6,8 +6,7 @@ var tabletojson = require('tabletojson');
 var rp = require('request-promise');
 const chalk = require('chalk');
 var nodemailer = require("nodemailer");
-
-
+var fs = require('fs');
 
 // var mysql = require('mysql');
 var Db = require('mysql-activerecord');
@@ -17,26 +16,8 @@ var moment = require('moment');
 const log = console.log;
 var app = express();
 
+eval(fs.readFileSync('config.js')+'');
 
-
-var db = new Db.Adapter({
-  server: 'localhost',
-  username: 'root',
-  password: '',
-  database: 'cbccvc_v4',
-  reconnectTimeout: 2000
-});
-
-var mailTransport = nodemailer.createTransport({
- host: 'mail.danang.gov.vn', 
- port: 587, 
-   secure: false,
-   requireTLS: true, // only use if the server really does support TLS
- auth: {
-   user: 'linhtv@danang.gov.vn',
-   pass: 'kh0ngaica'
- }  
-});
 
 var loginOptions = {
   
@@ -51,6 +32,94 @@ var loginOptions = {
   cookieUrl:"",	
 };
 
+async function compareData(id_thutuc) {
+  
+    console.log(chalk.bgBlue("!!!!!!!=============== COMPARE DATA ===========!!!!"));
+  
+    try{ 
+  
+      var options = {
+        
+        loginOptions,		
+        requestOptions:{
+          uri: 'http://csdl.thutuchanhchinh.vn/TTHC_UserControls/thutuc/pFormViewTTHC.aspx?ItemID='+id_thutuc,
+          
+          existDOM: '',
+          // If useCookieOnly is TRUE, no Login request will be sent if cannot get Cookie or Cookie expried. 
+          // {cookieFile} and {cookieDomain} must be set to get get Cookie file. 
+          // Other setting in loginOptions are ignored			
+          useCookieOnly:false, 
+          alwaysLogin:false,
+          reLoginWhenFail:1
+          
+        }		 
+      };
+            
+    
+      var data = await requestPrivate.getRequest(options);    
+      // console.log(data);
+      if (data instanceof Error) {
+        throw data;
+      }		
+  
+      const $ = cheerio.load(data,{ decodeEntities: false });
+      
+      noidung = '<table>'+$('.tab-detail').html()+'</table>'; // Toàn bộ Table chứa dữ liệu 
+
+      let noidung_origin = await new Promise((resolve,reject)=>{
+        db.select('noidung').where(' id_thutuc = "'+ id_thutuc + '" ')
+        .get('cchc_thutuc', function(err, results) {
+          
+          return void err ? reject(err) : resolve(results[0].noidung);
+        })   
+        
+      }); 
+
+      // log(noidung_origin);
+
+      emailContent = '<table border="1px"><tr><td>Nội dung lưu</td><td>Nội dung mới lấy</td><tr><td>'+noidung_origin+'</td><td>'+noidung+'</td></tr></table>';
+      var mailOptions = {
+        from: 'pmis@danang.gov.vn', 
+        to: 'linhtranvu@gmail.com',
+        html: emailContent, 
+      }      
+      
+      if(noidung_origin === noidung){ 
+        mailOptions.subject = 'MẪU CHƯA THAY ĐỔI '+moment().format('DD/MM');
+      }else{
+        mailOptions.subject = 'ERROR! MẪU ĐÃ THAY ĐỔI !!!'+moment().format('DD/MM');
+      }
+      mailTransport.sendMail(mailOptions,function(error,info){
+        log(info)
+      });
+
+    }catch(e){
+      
+      log(e); 
+  
+      var mailOptions = {
+        from: 'pmis@danang.gov.vn', 
+        to: 'linhtranvu@gmail.com,thaoptd@danang.gov.vn',
+        subject: 'Đồng bộ không thành công '+moment().format('DD/MM'),
+        html: e.toString() + '<p><p><p>' + JSON.stringify(thutuc,null, "\t"), 
+      };  
+  
+      mailTransport.sendMail(mailOptions,function(error,info){
+        // if(error) throw error;
+        console.log('Fail! Email sent: ' + e.toString());
+      console.log(error);
+        
+      }); 
+  
+      if(e.name === 'missingField'){
+      return e;
+        // return e.toString() + '. Đánh dấu đã đồng bộ Log';
+      }else{
+        return e;
+      }
+  
+    }
+  }
 
 async function saveThutuc(id_thutuc) {
 
@@ -64,7 +133,7 @@ async function saveThutuc(id_thutuc) {
       requestOptions:{
         uri: 'http://csdl.thutuchanhchinh.vn/TTHC_UserControls/thutuc/pFormViewTTHC.aspx?ItemID='+id_thutuc,
         
-        existDOM: '',
+        existDOM: '#thutucItemForm',
         // If useCookieOnly is TRUE, no Login request will be sent if cannot get Cookie or Cookie expried. 
         // {cookieFile} and {cookieDomain} must be set to get get Cookie file. 
         // Other setting in loginOptions are ignored			
@@ -85,33 +154,40 @@ async function saveThutuc(id_thutuc) {
     }		
 
     const $ = cheerio.load(data,{ decodeEntities: false });
+	
+	var noidung = '<table>'+$('.tab-detail').html()+'</table>'; // Toàn bộ Table chứa dữ liệu 
         
     $('#SelectVanBanTab').remove();
-    $("a").attr("href",'http://csdl.thutuchanhchinh.vn'+$("a").attr("href") );
+    $('a').each(function() {
+      var href = $(this).attr('href');
+      $(this).attr("href",'http://csdl.thutuchanhchinh.vn'+href );
+
+    });
+	$('#tblDVC').remove();
   
     //Nếu có Mẫu đơn, Tờ khai cũ thì xuất hiện thêm Dòng Mẫu đơn, dẫn đến vị trí DOM thay đổi
     if($('.tab-detail > tbody > tr:nth-child(10) > td.label').html().trim() == "Mẫu đơn, tờ khai" ){
     
-        $thoihan_field = 14; 
+        $thoihan_field = 15; 
+        $doituong_field = 16;
+        $donvi_field = 17;
+        $donviquyetdinh_field = 18;
+        $diachi_field = 19;
+        $donviphoihop_field = 20; //Chưa dùng
+        $donviuyquyen_field = 21; //Chưa dùng
+        $ketqua_field = 22;
+        $yeucau_field = 23;
+    
+    }else{
+        // maudon = "";	//Làm sạch field mẫu đơn (**TODO Nghiên cứu kỹ trước khi dùng)	
+        $thoihan_field = 14;
         $doituong_field = 15;
         $donvi_field = 16;
         $donviquyetdinh_field = 17;
         $diachi_field = 18;
-        $donviphoihop_field = 19; //Chưa dùng
-        $donviuyquyen_field = 20; //Chưa dùng
+    
         $ketqua_field = 21;
-        $yeucau_field = 22;
-    
-    }else{
-        // maudon = "";	//Làm sạch field mẫu đơn (**TODO Nghiên cứu kỹ trước khi dùng)	
-        $thoihan_field = 13;
-        $doituong_field = 14;
-        $donvi_field = 15;
-        $donviquyetdinh_field = 16;
-        $diachi_field = 17;
-    
-        $ketqua_field = 20;
-        $yeucau_field = 21;			
+        $yeucau_field = 22;			
     }		
     
     
@@ -161,8 +237,8 @@ async function saveThutuc(id_thutuc) {
       // ngayhieuluc : "",
       thoihan : myTrim($('.tab-detail > tbody > tr:nth-child('+$thoihan_field+') > td:nth-child(2)').html()),		
       doituong : myTrim($('.tab-detail > tbody > tr:nth-child('+$doituong_field+') > td:nth-child(2)').html()),	
-      donvi : myTrim($('.tab-detail > tbody > tr:nth-child('+$donvi_field+') > td:nth-child(2) > ul > li').html()),
-      donviquyetdinh : myTrim($('.tab-detail > tbody > tr:nth-child('+$donviquyetdinh_field+') > td.data > ul > li').html()),		
+      donvi : myTrim($('.tab-detail > tbody > tr:nth-child('+$donvi_field+') > td:nth-child(2) ').html()),
+      donviquyetdinh : myTrim($('.tab-detail > tbody > tr:nth-child('+$donviquyetdinh_field+') > td.data ').html()),		
       diachitiepnhan : myTrim($('.tab-detail > tbody > tr:nth-child('+$diachi_field+') > td.data').html()),				
       ketqua : myTrim($('.tab-detail > tbody > tr:nth-child('+$ketqua_field+') > td.data').html()),	
       yeucau : myTrim($('.tab-detail > tbody > tr:nth-child('+$yeucau_field+') > td.data').html()),		
@@ -177,8 +253,9 @@ async function saveThutuc(id_thutuc) {
       
       sync_date : moment().format('YYYY-MM-DD hh:mm:ss'),
       modified_date : moment().format('YYYY-MM-DD hh:mm:ss'),
+	  noidung : noidung,
       
-      // noidung : '<table>'+$('.tab-detail').html()+'</table>', // Toàn bộ Table chứa dữ liệu 
+      
     };
       
 		//------------- Kiểm tra các field dưới đây nếu trắng thì thoát và gửi mail lỗi.
@@ -250,7 +327,7 @@ async function saveThutuc(id_thutuc) {
       }else{
   
         var mailOptions = {
-          from: 'linhtv@danang.gov.vn',  
+          from: 'pmis@danang.gov.vn',  
           to: 'linhtranvu@gmail.com',
           subject: 'Đồng bộ thành công '+moment().format('DD/MM'),
           html: JSON.stringify(thutuc,null, "\t"),
@@ -259,11 +336,16 @@ async function saveThutuc(id_thutuc) {
   
       }
    
-      mailTransport.sendMail(mailOptions,function(error,info){
-        if(error) throw error;
-        console.log('Success! Email sent: ' + info.response);
+      await new Promise((resolve, reject) => { 
         
-      });        
+        mailTransport.sendMail(mailOptions,function(error,info){
+          console.log('Success! Email sent: ' + info.response);
+          return void error ? reject(error) : resolve(info)
+                    
+        });          
+
+      })
+      
     
       return info;
 
@@ -273,20 +355,22 @@ async function saveThutuc(id_thutuc) {
     log(e); 
 
     var mailOptions = {
-      from: 'linhtv@danang.gov.vn', 
-      to: 'linhtranvu@gmail.com',
+      from: 'pmis@danang.gov.vn', 
+      to: 'linhtranvu@gmail.com,thaoptd@danang.gov.vn',
       subject: 'Đồng bộ không thành công '+moment().format('DD/MM'),
       html: e.toString() + '<p><p><p>' + JSON.stringify(thutuc,null, "\t"), 
     };  
 
     mailTransport.sendMail(mailOptions,function(error,info){
       // if(error) throw error;
-      console.log('Fail! Email sent: ' + info.response);
+      console.log('Fail! Email sent: ' + e.toString());
+	  console.log(error);
       
     }); 
 
     if(e.name === 'missingField'){
-      return e.toString() + '. Đánh dấu đã đồng bộ Log';
+		return e;
+      // return e.toString() + '. Đánh dấu đã đồng bộ Log';
     }else{
       return e;
     }
@@ -334,7 +418,7 @@ function syncLog(){
 		requestOptions:{
 			uri: 'http://csdl.thutuchanhchinh.vn/TTHC_UserControls/AuditLog/pList.aspx?&Page=1&RowPerPage=100',
 			
-			existDOM: '',
+			existDOM: '#gridCoQuanBanHanhItem > table > tbody > tr:nth-child(2) > td:nth-child(2)',
 			// If useCookieOnly is TRUE, no Login request will be sent if cannot get Cookie or Cookie expried. 
 			// {cookieFile} and {cookieDomain} must be set to get get Cookie file. 
 			// Other setting in loginOptions are ignored			
@@ -367,8 +451,8 @@ function syncLog(){
             thaotac : $(this).find(' td:nth-child(3)').text().trim(),
             doituong : $(this).find(' td:nth-child(4)').text().trim(),
             tenbangghi : $(this).find(' td:nth-child(5)').text().trim(),
-            id_thutuc : $(this).find(' td:nth-child(6)').text().trim(),
-            timelog : converDate($(this).find(' td:nth-child(7)').text().trim()),
+            id_thutuc : $(this).find(' td:nth-child(7)').text().trim(),
+            timelog : converDate($(this).find(' td:nth-child(8)').text().trim()),
             sync_log_date : moment().format('YYYY-MM-DD hh:mm:ss'),        
           }
 
@@ -387,7 +471,7 @@ function syncLog(){
       
             }else{
               
-              // log(thutuc_log);
+              log(thutuc_log);
               db.insert('cchc_thutuc_log', thutuc_log, function(err, info) { 
                 log(info);
               });          
@@ -422,58 +506,89 @@ async function syncThutuc(){
   // data =  await saveThutuc(444360);   
 
 
-  results.forEach(function(thutuc_log) {
+  for (let thutuc_log of results){
 
-    // console.log(log);
-
-    (async function (){
-
-      log('Log thủ tục:',thutuc_log);
-     
-      data =  await saveThutuc(thutuc_log.id_thutuc);   
-      log('Update save Thu tuc: ', data);
-
-      if (data instanceof Error || data === undefined) {
- 
-        log('Error happend when save thu tuc: ',thutuc_log);
-
-      }else{
-        
-        logData = {
-          id : thutuc_log.id,
-          type: 1,
-          sync_thutuc_date:  moment().format('YYYY-MM-DD hh:mm:ss'),
-        } 
-
-        log(logData);
-          
-        var info = await new Promise((resolve, reject) => {        
-          db
-          .where(' id = '+ logData.id )  
-          .update('cchc_thutuc_log', logData, function(err, info) { 
-            log('Update log: ',info);
-            return void err ? reject(err) : resolve(info)
-          });    
+    log('Log thủ tục:',thutuc_log);
     
-        });          
+     data =  await saveThutuc(thutuc_log.id_thutuc);   
+     log('Update save Thu tuc: ', data);
+
+     if (data instanceof Error || data === undefined) {
+
+       log('Error happend when save thu tuc: ',thutuc_log);
+       logData = {
+         id : thutuc_log.id,
+         type: 0,
+         sync_thutuc_date:  moment().format('YYYY-MM-DD hh:mm:ss'),
+		 sync_info: data.toString(),
+       } 	   
+
+     }else{
+       
+       logData = {
+         id : thutuc_log.id,
+         type: 1,
+         sync_thutuc_date:  moment().format('YYYY-MM-DD hh:mm:ss'),
+		 sync_info: "",
+       } 
+	 }
+
+       log(logData);
+         
+       var info = await new Promise((resolve, reject) => {        
+         db
+         .where(' id = '+ logData.id )  
+         .update('cchc_thutuc_log', logData, function(err, info) { 
+           log('Update log: ',info);
+           return void err ? reject(err) : resolve(info)
+         });    
+   
+       });          
+   
+     	    
+
+  }
+  
+
+}
+
+async function compareMultipleData(){
+
+  if(moment().format('hh:mm') === '07:45' || moment().format('hh:mm') === '11:45'){
+
+    console.log(chalk.bgBlue("Begin compare"));
     
-      }	
-
-    })()
-
-  });
+      var sample = [447403, 444360, 444998]
+    
+    
+      for (let id_thutuc of sample){
+        await compareData(id_thutuc);
+      }    
+  }else{
+    console.log(chalk.bgBlue("Wating for right time"));
+    return;
+  }
   
 
 }
 
 
+// saveThutuc(447403);
+// saveThutuc(444360);
+ // saveThutuc(447431);
+
+// compareData(447403);
+
+
 // syncLog();
 
-syncThutuc();  
+// syncThutuc();  
 
-(function(){
+// compareMultipleData();
 
-  // setInterval(syncThutuc, 3000);
-  // setInterval(syncLog, 10000); //300000 = 5p
+//----------- RUN PRODUCTION ---------------//
+  setInterval(syncThutuc, 350000);
+  setInterval(syncLog, 300000); //300000 = 5p
 
-})();
+  setInterval(compareMultipleData, 60000); // 40s/lan
+
